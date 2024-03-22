6df58de66bdad9e9e7ee3e2f4bcbdd29e42d9254
--get <kumargauravgupta3@gmail.com>
2024-03-22 19:42:37 +0530

    adds internal moves and getSupported and getAttackedSquares methods

--- src/chess.ts
+++ src/chess.ts
@@ -57,7 +57,7 @@ export type Piece = {
   type: PieceSymbol
 }
 
-type InternalMove = {
+export type InternalMove = {
   color: Color
   from: number
   to: number
@@ -933,6 +933,7 @@ export class Chess {
 
     return false
   }
+  
 
   private _isKingAttacked(color: Color) {
     const square = this._kings[color]
@@ -2340,5 +2341,233 @@ export class Chess {
 
   moveNumber() {
     return this._moveNumber
+  }
+
+
+
+  _controlledAndSupportedSquares({
+    legal = true,
+    piece = undefined,
+    square = undefined,
+    color =  this.turn(),
+    attacking = false 
+  }: {
+    legal?: boolean
+    piece?: PieceSymbol
+    square?: Square,
+    color? : Color,
+    attacking? : boolean;
+  } = {}) {
+    const forSquare = square ? (square.toLowerCase() as Square) : undefined
+    const forPiece = piece?.toLowerCase()
+
+    const moves: InternalMove[] = []
+    const us = color;
+    const them = swapColor(us)
+
+    let firstSquare = Ox88.a8
+    let lastSquare = Ox88.h1
+    let singleSquare = false
+
+    // are we generating moves for a single square?
+    if (forSquare) {
+      // illegal square, return empty moves
+      if (!(forSquare in Ox88)) {
+        return []
+      } else {
+        firstSquare = lastSquare = Ox88[forSquare]
+        singleSquare = true
+      }
+    }
+
+    for (let from = firstSquare; from <= lastSquare; from++) {
+      // did we run off the end of the board
+      if (from & 0x88) {
+        from += 7
+        continue
+      }
+
+      // empty square or opponent, skip
+      if (!this._board[from] || this._board[from].color === them) {
+        continue
+      }
+      const { type } = this._board[from]
+
+      let to: number
+      if (type === PAWN) {
+        if (forPiece && forPiece !== type) continue
+
+        // single square, non-capturing
+        to = from + PAWN_OFFSETS[us][0]
+        //addMove(moves, us, from, to, PAWN)
+
+        // double square
+        to = from + PAWN_OFFSETS[us][1]
+        if (SECOND_RANK[us] === rank(from) && !this._board[to]) {
+        //addMove(moves, us, from, to, PAWN, undefined, BITS.BIG_PAWN)
+        }
+
+        // pawn support
+        for (let j = 2; j < 4; j++) {
+          to = from + PAWN_OFFSETS[us][j]
+          if (to & 0x88) continue
+          let matchColor = attacking ? them : us;
+          if (!this._board[to] || this._board[to]?.color === matchColor) {
+            addMove(
+              moves,
+              us,
+              from,
+              to,
+              PAWN,
+              this._board[to]?.type,
+              BITS.CAPTURE
+            )
+          } else if (to === this._epSquare) {
+            addMove(moves, us, from, to, PAWN, PAWN, BITS.EP_CAPTURE)
+          }
+        }
+      } else {
+        if (forPiece && forPiece !== type) continue
+
+        for (let j = 0, len = PIECE_OFFSETS[type].length; j < len; j++) {
+          const offset = PIECE_OFFSETS[type][j]
+          to = from
+
+          while (true) {
+            to += offset
+            if (to & 0x88) break
+
+            if (!this._board[to]) {
+              addMove(moves, us, from, to, type)
+            } else {
+              // own color, stop loop
+              //if (this._board[to].color === them) break
+
+              addMove(
+                moves,
+                us,
+                from,
+                to,
+                type,
+                this._board[to].type,
+                BITS.CAPTURE
+              )
+              break
+            }
+
+            /* break, if knight or king */
+            if (type === KNIGHT || type === KING) break
+          }
+        }
+      }
+    }
+
+    /*
+     * check for castling if we're:
+     *   a) generating all moves, or
+     *   b) doing single square move generation on the king's square
+     */
+
+    if (forPiece === undefined || forPiece === KING) {
+      if (!singleSquare || lastSquare === this._kings[us]) {
+        // king-side castling
+        if (this._castling[us] & BITS.KSIDE_CASTLE) {
+          const castlingFrom = this._kings[us]
+          const castlingTo = castlingFrom + 2
+
+          if (
+            !this._board[castlingFrom + 1] &&
+            !this._board[castlingTo] &&
+            !this._attacked(them, this._kings[us]) &&
+            !this._attacked(them, castlingFrom + 1) &&
+            !this._attacked(them, castlingTo)
+          ) {
+            addMove(
+              moves,
+              us,
+              this._kings[us],
+              castlingTo,
+              KING,
+              undefined,
+              BITS.KSIDE_CASTLE
+            )
+          }
+        }
+
+        // queen-side castling
+        if (this._castling[us] & BITS.QSIDE_CASTLE) {
+          const castlingFrom = this._kings[us]
+          const castlingTo = castlingFrom - 2
+
+          if (
+            !this._board[castlingFrom - 1] &&
+            !this._board[castlingFrom - 2] &&
+            !this._board[castlingFrom - 3] &&
+            !this._attacked(them, this._kings[us]) &&
+            !this._attacked(them, castlingFrom - 1) &&
+            !this._attacked(them, castlingTo)
+          ) {
+            addMove(
+              moves,
+              us,
+              this._kings[us],
+              castlingTo,
+              KING,
+              undefined,
+              BITS.QSIDE_CASTLE
+            )
+          }
+        }
+      }
+    }
+
+    /*
+     * return all pseudo-legal moves (this includes moves that allow the king
+     * to be captured)
+     */
+    if (!legal || this._kings[us] === -1) {
+      return moves
+    }
+
+    // filter out illegal moves
+    const legalMoves = []
+
+    for (let i = 0, len = moves.length; i < len; i++) {
+      //this._makeMove(moves[i])
+      if (!this._isKingAttacked(us)) {
+        legalMoves.push(moves[i])
+      }
+      this._undoMove()
+    }
+
+    return legalMoves.map((move) => this._makePretty(move))
+  }
+
+  getSupportedSquares({
+    legal = true,
+    piece = undefined,
+    square = undefined,
+    color =  this.turn()
+  }: {
+    legal?: boolean
+    piece?: PieceSymbol
+    square?: Square,
+    color? : Color
+  } = {}) {
+    return this._controlledAndSupportedSquares({legal, piece, square, color, attacking : false})
+  }
+
+  getAttackedSquares({
+    legal = true,
+    piece = undefined,
+    square = undefined,
+    color =  this.turn()
+  }: {
+    legal?: boolean
+    piece?: PieceSymbol
+    square?: Square,
+    color? : Color
+  } = {}) {
+    return this._controlledAndSupportedSquares({legal, piece, square, color, attacking : true})
   }
 }
